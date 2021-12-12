df726707f386955e18ffa25d3c72b965cbb4f629
Shambhavi Singh <78691639+shamvi-05@users.noreply.github.com>
2021-12-12 17:48:31 +0530

    updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,10 @@
+
+                import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:obliviate_app/model/user_model.dart';
+import 'package:obliviate_app/screens/login.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -8,44 +13,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+  @override
+  void initState() {
+
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value){  this.loggedInUser = UserModel.fromMap(value.data());
+         setState(() {
+
+         });
+
+  });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Obliviate"), centerTitle: true,),
-      body:Center(
-        child: Padding(
-          padding:EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                  height: 100,
-                  child: Image.asset("assets/logo1.png",
-                      fit: BoxFit.contain)),
-              Text("say yes to create memories & cherish it", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              SizedBox(height:10),
-              Text(
-                "Name",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                )),
-              Text(
-                  "Email",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  )),
-              SizedBox(height:10),
-              ActionChip(label: Text("Logou"), onPressed: (){}),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Text("Obliviate",), centerTitle: true,foregroundColor: Colors.teal,),
+        body:Center(
+          child: Padding(
+            padding:EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                    height: 100,
+                    child: Image.asset("assets/logo1.png",
+                        fit: BoxFit.contain)),
+                Text("Create memories and cherish it!", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                SizedBox(height:10),
+                Text(
+                    "${loggedInUser.firstName} ${loggedInUser.secondName}",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Text(
+                    "${loggedInUser.email}",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(height:10),
+                ActionChip(label: Text("Logout"), onPressed: (){
+                  logout(context);
+                }),
 
-            ],
+              ],
+            ),
           ),
-        ),
 
-      )
+        )
 
     );
   }
+
+  Future<void> logout(BuildContext context) async{
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context)=>LoginScreen()));
+  }
+
+
 }
