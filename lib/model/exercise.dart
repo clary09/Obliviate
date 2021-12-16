06447592e8bbdb0d14af5ecac1a73655e3bb8604
Shambhavi Singh <78691639+shamvi-05@users.noreply.github.com>
2021-12-16 19:19:27 +0530

    exercises commit

---
+++ lib/model/exercise.dart
@@ -0,0 +1,36 @@
+class Exercise{
+   final String name,img , tt, descrip;
+  Exercise({required this.name,
+    required this.img,
+    required this.tt,
+    required this.descrip
+  });
+}
+final exer = [Exercise(
+    name: "Marching",
+    img: 'assets/exercise/e1.png',
+    tt: "20 mins",
+    descrip: "Endurance/Aerobic"),
+
+  Exercise(
+      name: "Marching",
+      img: 'assets/exercise/e1.png',
+      tt: "20 mins",
+      descrip: "Endurance/Aerobic"),
+
+  Exercise(
+      name: "Marching",
+      img: 'assets/exercise/e1.png',
+      tt: "20 mins",
+      descrip: "Endurance/Aerobic"),
+
+  // Exercise(
+  //     name: "Marching",
+  //     img: 'assets/exercise/e1.png',
+  //     tt: "20 mins",
+  //     descrip: "Endurance/Aerobic"),
+
+
+];
+
+
