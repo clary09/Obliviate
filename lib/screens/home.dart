88e31be65ecb8f86ea64d3700954a998bdc9b7af
Ain Fatima <82439481+clary09@users.noreply.github.com>
2021-12-22 14:44:32 +0530

    added family gallery button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:obliviate_app/screens/health.dart';
-
+import 'package:obliviate_app/screens/family.dart';
 import 'package:obliviate_app/screens/profile.dart';
 import 'package:obliviate_app/screens/recreation.dart';
 class SideDrawer extends StatelessWidget {
@@ -243,7 +243,8 @@ class _MainPageState extends State<MainPage> {
                 child: TextButton(
                   onPressed: ()
                   {
-
+                     Navigator.push(context, MaterialPageRoute(
+                   builder: (context) => FamilyScreen()));
                   },
 
                  child: Text( "Family Gallery",
